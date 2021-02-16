2c7ae9655bd702ec90f6d81de77febaca4d3b20b
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-02-16 17:55:37 +0100

    simplify

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -1,11 +1,7 @@
 import numpy as np
 
 from benchopt import BaseDataset
-from benchopt import safe_import_context
 from benchopt.datasets.simulated import make_correlated_data
-
-with safe_import_context() as import_ctx:
-    from sklearn.model_selection import train_test_split
 
 
 class Dataset(BaseDataset):
@@ -18,9 +14,6 @@ class Dataset(BaseDataset):
         ]
     }
 
-    install_cmd = 'conda'
-    requirements = ['scikit-learn']
-
     def __init__(self, n_samples=1_000, n_features=2, rho=0., random_state=42):
         self.n_samples = n_samples
         self.n_features = n_features
@@ -28,19 +21,18 @@ class Dataset(BaseDataset):
         self.random_state = random_state
 
     def get_data(self):
-        rng = np.random.RandomState(self.random_state)
-
         n_samples = 2 * self.n_samples  # take half of train and half for test
         X, y, _ = make_correlated_data(n_samples, self.n_features,
-                                       rho=self.rho, random_state=rng)
+                                       rho=self.rho,
+                                       random_state=self.random_state)
 
         # make it balanced classification
         y -= np.mean(y)
         y = np.sign(y)
 
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.5, random_state=42
-        )
+        # Split train and test
+        X_train, X_test = X[:self.n_samples], X[self.n_samples:]
+        y_train, y_test = y[:self.n_samples], y[self.n_samples:]
 
         data = dict(X_train=X_train, X_test=X_test,
                     y_train=y_train, y_test=y_test)
